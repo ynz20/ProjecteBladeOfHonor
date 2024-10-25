dadf30646535a19bf4a5bd2f26690fc4d763492f
Yasser <yasser.zuaki@cirvianum.cat>
2024-10-25 16:15:47 +0200

    LOGIN finalizat

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.html", "./index.html"],
+  content: ["./public/**/*.html", "./index.html", "./src/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'black-japan': '#2D2D2D',
+        'white-japan': '#F3F2E0' ,
+        'red-japan': '#8A0303'
+      },
+      fontFamily: {
+        serif: ['EB Garamond', 'serif'],
+      },
+    },
+
   },
   plugins: [],
 };
