7018d9a5270561b154f459fd835df1d606d69d22
Luis <luis.ruizi@cirvianum.cat>
2024-10-25 16:13:41 +0200

    Home Page acabada

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.html", "./index.html", "./src/**/*.html"],
+  content: [
+    './index.html',          // Incluir index.html en la raíz
+    './src/**/*.html',       // Incluir todos los archivos HTML en src
+    './public/**/*.html'     // Incluir todos los archivos HTML en public (si es necesario)
+  ],
   theme: {
     extend: {},
     colors: {
       'black-japan': '#2D2D2D',
-      'white-japan': '#F3F2E0' ,
+      'white-japan': '#F3F2E0',
       'red-japan': '#8A0303'
-    }
+    },
+    fontFamily: {
+      serif: ['EB Garamond', 'serif'],
+    },
   },
   plugins: [],
 };
