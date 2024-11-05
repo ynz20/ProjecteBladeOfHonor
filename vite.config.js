241b0e9c3c88c7f3e5621171e95b029c4c74b535
Luis <luis.ruizi@cirvianum.cat>
2024-11-05 20:06:59 +0100

    ajustem el vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,28 @@
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
-  root: ".", // Root directory for Vite
+  root: "./", // Directori root de Vite
+  base: "./", // Rutes relatives per als assets
+  build: {
+    outDir: "dist", // Directori de sortida per al build
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"), // Pàgina principal
+        login: path.resolve(__dirname, "src/login.html"),
+        charity: path.resolve(__dirname, "src/charityworkpage.html"),
+        contactus: path.resolve(__dirname, "src/contactus.html"),
+        dojo: path.resolve(__dirname, "src/dojo.html"),
+        dojoquotes: path.resolve(__dirname, "src/dojoquotes.html"),
+        home: path.resolve(__dirname, "src/home.html"),
+        realkatanas: path.resolve(__dirname, "src/realkatanas.html"),
+        signup: path.resolve(__dirname, "src/signup.html"),
+      },
+    },
+  },
   server: {
     watch: {
-      usePolling: true, // Ensures changes are detected in some environments
+      usePolling: true, // Per assegurar que els canvis es detecten en alguns entorns
     },
   },
 });
